5b6a91329a4c415c9a85ecfa6d2f48986f768704
bongsh0112 <sehwan@bongsehwan-ui-MacBookAir.local>
2023-06-03 00:49:58 +0900

    feat: knock not found error 추가

--- Domain/src/main/java/tify/server/domain/domains/user/exception/DailyQuestionException.java
+++ Domain/src/main/java/tify/server/domain/domains/user/exception/DailyQuestionException.java
@@ -11,7 +11,8 @@ import tify.server.core.exception.BaseErrorCode;
 @AllArgsConstructor
 public enum DailyQuestionException implements BaseErrorCode {
     DAILYQUESTION_NOT_FOUND_ERROR(NOT_FOUND, "DailyQuestion_404_1", "일일 질문 정보를 찾을 수 없습니다."),
-    ANSWER_NOT_FOUND_ERROR(NOT_FOUND, "Answer_404_1", "일일 질문에 대한 답변 정보를 찾을 수 없습니다.");
+    ANSWER_NOT_FOUND_ERROR(NOT_FOUND, "Answer_404_1", "일일 질문에 대한 답변 정보를 찾을 수 없습니다."),
+    KNOCK_NOT_FOUND_ERROR(NOT_FOUND, "Knock_404_1","찌르기에 대한 답변 정보를 찾을 수 없습니다.");
 
     private final Integer statusCode;
     private final String errorCode;
