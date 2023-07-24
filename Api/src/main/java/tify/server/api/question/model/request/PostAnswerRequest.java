594a72be4b44da1d49ad0b0647e768821ccad933
김민준 <sanbonai06@naver.com>
2023-07-24 18:33:00 +0900

    fix: 길이 validation 수정

--- Api/src/main/java/tify/server/api/question/model/request/PostAnswerRequest.java
+++ Api/src/main/java/tify/server/api/question/model/request/PostAnswerRequest.java
@@ -2,8 +2,8 @@ package tify.server.api.question.model.request;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +13,6 @@ public class PostAnswerRequest {
 
     @Schema(description = "답변")
     @NotBlank(message = "답변을 입력해주세요.")
-    @Max(value = 40, message = "최대 40자까지만 입력 가능합니다.")
+    @Size(max = 40, message = "최대 40자까지만 입력 가능합니다.")
     private String answer;
 }
