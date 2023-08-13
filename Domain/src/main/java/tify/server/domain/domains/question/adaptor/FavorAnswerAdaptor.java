c70f4502c2f3aa6a6bfb89abe510d12daabdbe9e
bongsh0112 <bongsh0112@gmail.com>
2023-08-13 15:14:44 +0900

    feat: FavorAnswer 테이블 생성

---
+++ Domain/src/main/java/tify/server/domain/domains/question/adaptor/FavorAnswerAdaptor.java
@@ -0,0 +1,22 @@
+package tify.server.domain.domains.question.adaptor;
+
+
+import lombok.RequiredArgsConstructor;
+import tify.server.core.annotation.Adaptor;
+import tify.server.domain.domains.question.domain.FavorAnswer;
+import tify.server.domain.domains.question.exception.AnswerNotFoundException;
+import tify.server.domain.domains.question.repository.FavorAnswerRepository;
+
+@Adaptor
+@RequiredArgsConstructor
+public class FavorAnswerAdaptor {
+
+    private final FavorAnswerRepository favorAnswerRepository;
+
+    public FavorAnswer query(Long favorAnswerId) {
+        return favorAnswerRepository
+                .findById(favorAnswerId)
+                .orElseThrow(() -> AnswerNotFoundException.EXCEPTION);
+        //            .orElseThrow(() -> FavorAnswerNotFoundException.EXCEPTION);
+    }
+}
