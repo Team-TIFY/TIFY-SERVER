aa68ed2354b43e3410be345d91f3a70cf852b66f
SeHwan Bong <bongsh0112@gmail.com>
2023-08-22 16:05:13 +0900

    feat: 친구 프로필 정보 조회 API 작성 (#29)
    * feat: 친구 프로필 정보 조회 API 초안
    
    * spotless apply
    
    * edit: neighbor validator, 에러 코드 수정

--- Api/src/main/java/tify/server/api/user/controller/UserController.java
+++ Api/src/main/java/tify/server/api/user/controller/UserController.java
@@ -10,11 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tify.server.api.user.model.dto.request.PutUserProfileRequest;
 import tify.server.api.user.model.dto.request.UserOnBoardingRequest;
-import tify.server.api.user.service.UpdateUserProfileUseCase;
-import tify.server.api.user.service.UserFavorFilterUseCase;
-import tify.server.api.user.service.UserFavorUseCase;
-import tify.server.api.user.service.UserInfoUseCase;
-import tify.server.api.user.service.UserOnBoardingUseCase;
+import tify.server.api.user.service.*;
 import tify.server.domain.common.vo.UserFavorVo;
 import tify.server.domain.common.vo.UserInfoVo;
 import tify.server.domain.common.vo.UserProfileVo;
@@ -33,12 +29,19 @@ public class UserController {
     private final UpdateUserProfileUseCase updateUserProfileUseCase;
     private final UserFavorFilterUseCase userFavorFilterUseCase;
     private final UserOnBoardingUseCase userOnBoardingUseCase;
+    private final NeighborInfoUseCase neighborInfoUseCase;
 
     // userId를 pathvariable로 받아서 그 해당 유저의 profile 정보를 리턴하기.
     @Operation(summary = "유저 정보 조회")
     @GetMapping("/{userId}")
     public UserProfileVo getUserProfileInfo(@PathVariable Long userId) {
         return userInfoUseCase.execute(userId);
+    }
+
+    @Operation(summary = "친구 정보 조회")
+    @GetMapping("/{neighborId}")
+    public UserProfileVo getNeighborProfileInfo(@PathVariable Long neighborId) {
+        return neighborInfoUseCase.execute(neighborId);
     }
 
     @Operation(summary = "내 취향 태그 조회")
--- Domain/src/main/java/tify/server/domain/domains/user/validator/UserValidator.java
+++ Domain/src/main/java/tify/server/domain/domains/user/validator/UserValidator.java
@@ -1,10 +1,12 @@
 package tify.server.domain.domains.user.validator;
 
 import static tify.server.domain.domains.user.exception.UserException.ALREADY_EXIST_USER_ERROR;
+import static tify.server.domain.domains.user.exception.UserException.NOT_NEIGHBOR_ERROR;
 
 import lombok.RequiredArgsConstructor;
 import tify.server.core.annotation.Validator;
 import tify.server.core.exception.BaseException;
+import tify.server.domain.domains.user.adaptor.NeighborAdaptor;
 import tify.server.domain.domains.user.adaptor.UserAdaptor;
 import tify.server.domain.domains.user.domain.OauthInfo;
 
@@ -13,10 +15,17 @@ import tify.server.domain.domains.user.domain.OauthInfo;
 public class UserValidator {
 
     private final UserAdaptor userAdaptor;
+    private final NeighborAdaptor neighborAdaptor;
 
     public void isNewUser(OauthInfo oauthInfo) {
         if (userAdaptor.existByOauthInfo(oauthInfo)) {
             throw new BaseException(ALREADY_EXIST_USER_ERROR);
         }
+    }
+
+    public void isNeighbor(Long userId, Long neighborId) {
+        neighborAdaptor
+                .queryByFromUserIdAndToUserId(userId, neighborId)
+                .orElseThrow(() -> new BaseException(NOT_NEIGHBOR_ERROR));
     }
 }
