715399915324e06938415b07431bfd6a2b46b62a
bongsh0112 <bongsh0112@gmail.com>
2023-08-31 14:40:57 +0900

    fix: RetrieveNeighborDTO Constructor 설정

--- Domain/src/main/java/tify/server/domain/domains/user/dto/model/RetrieveNeighborDTO.java
+++ Domain/src/main/java/tify/server/domain/domains/user/dto/model/RetrieveNeighborDTO.java
@@ -1,11 +1,11 @@
 package tify.server.domain.domains.user.dto.model;
 
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@Builder
+@AllArgsConstructor
 public class RetrieveNeighborDTO {
 
     private Long neighborId;
