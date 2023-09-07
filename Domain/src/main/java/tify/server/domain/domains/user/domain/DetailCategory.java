627280502446a3a29a7418362f89627e51b61803
bongsh0112 <bongsh0112@gmail.com>
2023-09-07 17:25:05 +0900

    feat: DetailCategory Enum값 추가

--- Domain/src/main/java/tify/server/domain/domains/user/domain/DetailCategory.java
+++ Domain/src/main/java/tify/server/domain/domains/user/domain/DetailCategory.java
@@ -1,5 +1,7 @@
 package tify.server.domain.domains.user.domain;
 
+import static tify.server.domain.domains.user.domain.SmallCategory.CLOTHES;
+import static tify.server.domain.domains.user.domain.SmallCategory.FASHION_PRODUCT;
 import static tify.server.domain.domains.user.domain.SmallCategory.FRAGRANCE;
 import static tify.server.domain.domains.user.domain.SmallCategory.MAKEUP;
 
@@ -12,6 +14,12 @@ public enum DetailCategory {
     LIP("립", MAKEUP),
     EYE("아이", MAKEUP),
     PERFUME("향수", FRAGRANCE),
+    MOISTURE("보습겸용", FRAGRANCE),
+    PLACE("공간향", FRAGRANCE),
+    TOP("상의", CLOTHES),
+    FAS_PRODUCT("패션소품", FASHION_PRODUCT),
+    DIG_PRODUCT("디지털/테크소품", FASHION_PRODUCT),
+    BAG("가방", SmallCategory.BAG),
     ;
 
     final String value;
