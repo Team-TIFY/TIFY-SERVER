d1b1b57545995adac203b1ff1443d2f69d9f4f72
bongsh0112 <bongsh0112@gmail.com>
2023-09-24 11:37:43 +0900

    edit: Transactional 추가 #49

--- Api/src/main/java/tify/server/api/product/service/ProductSearchUseCase.java
+++ Api/src/main/java/tify/server/api/product/service/ProductSearchUseCase.java
@@ -4,6 +4,7 @@ package tify.server.api.product.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.transaction.annotation.Transactional;
 import tify.server.api.common.slice.SliceResponse;
 import tify.server.core.annotation.UseCase;
 import tify.server.domain.domains.product.adaptor.ProductAdaptor;
@@ -16,6 +17,7 @@ public class ProductSearchUseCase {
 
     private final ProductAdaptor productAdaptor;
 
+    @Transactional(readOnly = true)
     public SliceResponse<ProductRetrieveDTO> execute(String keyword, Pageable pageable) {
         ProductCondition productCondition = new ProductCondition(keyword, pageable);
         Slice<ProductRetrieveDTO> products = productAdaptor.searchByKeyword(productCondition);
