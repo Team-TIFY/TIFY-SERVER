377da8ec799864f11499c889052989eddff308d1
bongsh0112 <bongsh0112@gmail.com>
2023-09-25 11:04:35 +0900

    edit: Transactional 추가 #51

--- Api/src/main/java/tify/server/api/user/service/RetrieveBirthdayNeighborUseCase.java
+++ Api/src/main/java/tify/server/api/user/service/RetrieveBirthdayNeighborUseCase.java
@@ -4,6 +4,7 @@ package tify.server.api.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.transaction.annotation.Transactional;
 import tify.server.api.common.slice.SliceResponse;
 import tify.server.api.utils.UserUtils;
 import tify.server.core.annotation.UseCase;
@@ -18,6 +19,7 @@ public class RetrieveBirthdayNeighborUseCase {
     private final NeighborAdaptor neighborAdaptor;
     private final UserUtils userUtils;
 
+    @Transactional(readOnly = true)
     public SliceResponse<RetrieveNeighborDTO> execute(Pageable pageable) {
         Long currentUserId = userUtils.getUserId();
         NeighborCondition neighborCondition = new NeighborCondition(currentUserId, pageable);
