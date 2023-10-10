5bcc241a348bb34bd4b5c5c9d0734e52554402d6
bongsh0112 <bongsh0112@gmail.com>
2023-10-10 23:13:53 +0900

    edit : userProfileVo에 userId 추가 #61

--- Domain/src/main/java/tify/server/domain/common/vo/UserProfileVo.java
+++ Domain/src/main/java/tify/server/domain/common/vo/UserProfileVo.java
@@ -12,6 +12,8 @@ public class UserProfileVo {
 
     private final String userName;
 
+    private final String userId;
+
     private final String email;
 
     private final String thumbnail;
@@ -27,6 +29,7 @@ public class UserProfileVo {
     public static UserProfileVo from(User user) {
         return UserProfileVo.builder()
                 .userName(user.getProfile().getUserName())
+                .userId(user.getUserId())
                 .email(user.getProfile().getEmail())
                 .thumbnail(user.getProfile().getThumbNail())
                 .birth(user.getProfile().getBirth())
