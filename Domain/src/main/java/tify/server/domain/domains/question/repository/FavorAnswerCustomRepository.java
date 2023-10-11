457463b55fee155298ee356bc3fee2bdcb813437
bongsh0112 <bongsh0112@gmail.com>
2023-10-12 01:22:34 +0900

    feat: 중분류 별 사용자의 취향 답변 조회 필터 구현

--- Domain/src/main/java/tify/server/domain/domains/question/repository/FavorAnswerCustomRepository.java
+++ Domain/src/main/java/tify/server/domain/domains/question/repository/FavorAnswerCustomRepository.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.Optional;
 import tify.server.domain.domains.question.domain.FavorAnswer;
 import tify.server.domain.domains.question.dto.model.FavorAnswerCategoryDto;
+import tify.server.domain.domains.question.dto.model.FavorAnswerVo;
+import tify.server.domain.domains.user.domain.SmallCategory;
 
 public interface FavorAnswerCustomRepository {
 
     List<FavorAnswerCategoryDto> searchToAnswerCategory(Long currentUserId);
 
     Optional<FavorAnswer> searchByCategoryAndNumber(Long userId, String category, Long number);
+
+    List<FavorAnswerVo> getFavorAnswerBySmallCategory(Long userId, SmallCategory smallCategory);
 }
