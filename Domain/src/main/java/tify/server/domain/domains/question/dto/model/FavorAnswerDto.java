e631d8f97508194d5b0eaf62197beb727a5829d8
SeHwan Bong <bongsh0112@gmail.com>
2023-10-11 13:30:40 +0900

    FavorAnswerDto 답변 @NotBlank 삭제 (#64)
    * edit : FavorAnswerDto 답변 @NotBlank 삭제 #63
    
    * spotless apply #63

--- Domain/src/main/java/tify/server/domain/domains/question/dto/model/FavorAnswerDto.java
+++ Domain/src/main/java/tify/server/domain/domains/question/dto/model/FavorAnswerDto.java
@@ -1,7 +1,6 @@
 package tify.server.domain.domains.question.dto.model;
 
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +12,5 @@ public class FavorAnswerDto {
     @NotNull(message = "카테고리별 질문 순서를 입력해주세요.")
     private int num;
 
-    @NotBlank(message = "답변 내용을 입력해주세요.")
     private String answer;
 }
