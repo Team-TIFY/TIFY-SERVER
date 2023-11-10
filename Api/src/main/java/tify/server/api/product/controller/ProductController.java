3750fd3d6e3405dbd177501631b3c5624f517906
bongsh0112 <bongsh0112@gmail.com>
2023-11-10 16:13:49 +0900

    feat : 상품 리스트 조회 시 가격순, 가격대별 필터 Parameter 수정 #84

--- Api/src/main/java/tify/server/api/product/controller/ProductController.java
+++ Api/src/main/java/tify/server/api/product/controller/ProductController.java
@@ -2,10 +2,8 @@ package tify.server.api.product.controller;
 
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springdoc.api.annotations.ParameterObject;
@@ -21,10 +19,7 @@ import tify.server.api.product.model.dto.ProductFilterCondition;
 import tify.server.api.product.service.CrawlingUseCase;
 import tify.server.api.product.service.ProductSearchUseCase;
 import tify.server.api.product.service.RetrieveProductListUseCase;
-import tify.server.domain.domains.product.domain.PriceFilter;
-import tify.server.domain.domains.product.domain.PriceOrder;
 import tify.server.domain.domains.product.dto.ProductRetrieveDTO;
-import tify.server.domain.domains.user.domain.SmallCategory;
 
 @RestController
 @Slf4j
@@ -67,16 +62,8 @@ public class ProductController {
     @Operation(summary = "SmallCategory(FE기준 중분류) 별 상품을 조회합니다.")
     @GetMapping("/products/small-category")
     public SliceResponse<ProductRetrieveDTO> getCategoricalProduct(
-            @Schema(description = "필터로 쓸 중분류입니다.") @RequestParam
-                    List<SmallCategory> smallCategoryList,
-            @Schema(description = "기본, 가격낮은순, 가격높은순 정렬입니다.", implementation = PriceOrder.class)
-                    @RequestParam
-                    PriceOrder priceOrder,
-            @Schema(description = "가격대별 필터입니다.", implementation = PriceFilter.class) @RequestParam
-                    PriceFilter priceFilter,
+            @ParameterObject ProductFilterCondition productFilterCondition,
             @ParameterObject @PageableDefault(size = 10) Pageable pageable) {
-        ProductFilterCondition productFilterCondition =
-                new ProductFilterCondition(smallCategoryList, priceOrder, priceFilter);
         return retrieveProductListUseCase.executeToSmallCategory(
                 productFilterCondition.getSmallCategoryList(),
                 productFilterCondition.getPriceOrder(),
--- Api/src/main/java/tify/server/api/product/model/dto/ProductFilterCondition.java
+++ Api/src/main/java/tify/server/api/product/model/dto/ProductFilterCondition.java
@@ -11,7 +11,7 @@ import tify.server.domain.domains.user.domain.SmallCategory;
 @Data
 public class ProductFilterCondition {
 
-    @Schema(description = "필터로 쓸 중분류입니다.", implementation = SmallCategory.class)
+    @Schema(description = "필터로 쓸 중분류입니다.")
     private final List<SmallCategory> smallCategoryList;
 
     @Schema(description = "기본, 가격낮은순, 가격높은순 정렬입니다.", implementation = PriceOrder.class)
