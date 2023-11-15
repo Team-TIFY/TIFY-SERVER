c01fb9fd2c8482c7298b9e1f76ad34342bf8a9aa
bongsh0112 <bongsh0112@gmail.com>
2023-11-15 14:17:27 +0900

    feat : BMPER 추천 전략 구현 #100

--- Domain/src/main/java/tify/server/domain/domains/question/strategy/BMPERRecommendationStrategy.java
+++ Domain/src/main/java/tify/server/domain/domains/question/strategy/BMPERRecommendationStrategy.java
@@ -20,7 +20,34 @@ public class BMPERRecommendationStrategy implements ProductRecommendationStrateg
     @Override
     public List<Product> recommendation(
             Long userId, String categoryName, List<FavorRecommendationDTO> dto) {
-        return null;
+
+        List<FavorRecommendationDTO> recommendationDTO = getRecommendationDTO(userId);
+        List<Product> productList = new ArrayList<>();
+
+        /**
+         * 1번 스텝(좋아하는 향기의 강도?)
+         * favorRecommendationDTO의 첫번째 질문의 답변을 기반으로 한 퍼퓸, 오드퍼퓸....으로 상품 검색하여 productList에 addAll
+         */
+        productList.addAll(filterStep(CATEGORY_NAME, recommendationDTO.get(0).getAnswer()));
+
+        /**
+         * 2번 스텝(내가 원하는 나의 이미지?)
+         * 객관식 2개까지 가능이기 때문에 답변 개수별로 경우를 나눔
+         */
+        String[] splitAnswer = recommendationDTO.get(1).getAnswer().split(", ");
+        if (splitAnswer.length > 1) {
+            return productList.stream()
+                .filter(product -> product.getCharacteristic().contains(splitAnswer[0]))
+                .filter(product -> product.getCharacteristic().contains(splitAnswer[1]))
+                .toList();
+        } else {
+            return productList.stream().filter(product -> product.getCharacteristic()
+                .contains(recommendationDTO.get(1).getAnswer())).toList();
+        }
+    }
+
+    private List<Product> filterStep(String categoryName, String answer) {
+        return productAdaptor.queryAllByCategoryNameAndCharacter(categoryName, answer);
     }
 
     private List<FavorRecommendationDTO> getRecommendationDTO(Long userId) {
