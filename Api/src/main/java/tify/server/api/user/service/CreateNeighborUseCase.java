a32c23d72455e7f918c72819cf4941a4e90ff31d
bongsh0112 <bongsh0112@gmail.com>
2023-12-16 11:33:01 +0900

    spotless apply

--- Api/src/main/java/tify/server/api/user/service/CreateNeighborUseCase.java
+++ Api/src/main/java/tify/server/api/user/service/CreateNeighborUseCase.java
@@ -1,14 +1,12 @@
 package tify.server.api.user.service;
 
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import tify.server.api.config.security.SecurityUtils;
 import tify.server.core.annotation.UseCase;
 import tify.server.domain.domains.user.adaptor.NeighborAdaptor;
 import tify.server.domain.domains.user.adaptor.UserAdaptor;
-import tify.server.domain.domains.user.domain.Neighbor;
 import tify.server.domain.domains.user.domain.NeighborApplication;
 import tify.server.domain.domains.user.domain.User;
 import tify.server.domain.domains.user.exception.AlreadyExistNeighborRelationshipException;
