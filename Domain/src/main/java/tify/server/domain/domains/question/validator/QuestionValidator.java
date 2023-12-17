e3a533f91744179e4dd819274eee433c958ef2d7
bongsh0112 <bongsh0112@gmail.com>
2023-12-17 14:30:24 +0900

    fix : 다른 날짜의 데일리 질문에도 답변을 남길 수 있도록 수정

--- Domain/src/main/java/tify/server/domain/domains/question/validator/QuestionValidator.java
+++ Domain/src/main/java/tify/server/domain/domains/question/validator/QuestionValidator.java
@@ -1,13 +1,11 @@
 package tify.server.domain.domains.question.validator;
 
 
-import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import tify.server.core.annotation.Validator;
 import tify.server.domain.domains.question.adaptor.AnswerAdaptor;
 import tify.server.domain.domains.question.adaptor.DailyQuestionAdaptor;
 import tify.server.domain.domains.question.exception.AlreadyAnsweredQuestionException;
-import tify.server.domain.domains.question.exception.NotValidTodayQuestionException;
 
 @Validator
 @RequiredArgsConstructor
@@ -17,11 +15,6 @@ public class QuestionValidator {
     private final AnswerAdaptor answerAdaptor;
 
     public void isValidateAnswerToQuestion(Long questionId, Long userId) {
-        // 질문이 오늘 날짜의 질문이 맞는지
-        LocalDate now = LocalDate.now();
-        if (!dailyQuestionAdaptor.query(questionId).getLoadingDate().isEqual(now)) {
-            throw NotValidTodayQuestionException.EXCEPTION;
-        }
         // 유저가 이미 해당 질문에 대한 답을 남겼는지
         if (answerAdaptor.optionalQueryByQuestionAndUser(questionId, userId).isPresent()) {
             throw AlreadyAnsweredQuestionException.EXCEPTION;
