e43461fdcdb28befb054aff07b322af2b2c40472
SeHwan Bong <bongsh0112@gmail.com>
2023-12-18 22:21:47 +0900

    [FIX] 온보딩, userFavor 관련 API 수정 (#137)
    * fix : UserTag, UserFavor 매핑 관계 검토 #134
    
    * fix : UserFavor 관련 api 에러 수정 #134

--- Domain/src/main/java/tify/server/domain/domains/user/domain/User.java
+++ Domain/src/main/java/tify/server/domain/domains/user/domain/User.java
@@ -8,12 +8,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import tify.server.domain.domains.AbstractTimeStamp;
 import tify.server.domain.domains.user.vo.UserInfoVo;
 import tify.server.domain.domains.user.vo.UserProfileVo;
 
-@Slf4j
 @Getter
 @Entity
 @Table(name = "tbl_user")
@@ -93,7 +91,8 @@ public class User extends AbstractTimeStamp {
         this.profile = Profile.builder().userName(username).birth(birth).gender(gender).build();
         this.userId = userId;
         this.onBoardingStatus = onBoardingStatus;
-        this.userFavors = userFavorList;
+        this.userFavors.clear();
+        this.userFavors.addAll(userFavorList);
     }
 
     public void updateFavor() {
@@ -101,7 +100,8 @@ public class User extends AbstractTimeStamp {
     }
 
     public void updateUserFavors(List<UserFavor> userFavors) {
-        this.userFavors = userFavors;
+        this.userFavors.clear();
+        this.userFavors.addAll(userFavors);
     }
 
     public void updateAppleRefreshToken(String appleRefreshToken) {
