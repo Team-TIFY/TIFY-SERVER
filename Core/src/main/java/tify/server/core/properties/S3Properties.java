1337618ea93ee1b4f6d0ede57b24a7bc9ba221cf
bongsh0112 <bongsh0112@gmail.com>
2023-12-29 19:52:03 +0900

    feat : s3 설정 2트

--- Core/src/main/java/tify/server/core/properties/S3Properties.java
+++ Core/src/main/java/tify/server/core/properties/S3Properties.java
@@ -4,9 +4,11 @@ package tify.server.core.properties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
 @AllArgsConstructor
+@ConstructorBinding
 @ConfigurationProperties(prefix = "s3")
 public class S3Properties {
 
