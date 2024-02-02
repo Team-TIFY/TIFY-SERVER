29c1bae8526fb4b03b1234dbb5758d563f028e6d
bongsh0112 <bongsh0112@gmail.com>
2024-02-03 00:21:13 +0900

    FAACC 추천 전략 로직 수정 #138

--- Domain/src/main/java/tify/server/domain/domains/question/strategy/FAACCRecommendationStrategy.java
+++ Domain/src/main/java/tify/server/domain/domains/question/strategy/FAACCRecommendationStrategy.java
@@ -6,16 +6,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import tify.server.core.exception.BaseException;
 import tify.server.domain.domains.product.adaptor.ProductAdaptor;
 import tify.server.domain.domains.product.domain.Product;
 import tify.server.domain.domains.question.adaptor.FavorAnswerAdaptor;
 import tify.server.domain.domains.question.domain.FavorAnswer;
 import tify.server.domain.domains.question.dto.condition.FavorRecommendationDTO;
-import tify.server.domain.domains.question.exception.QuestionException;
 
 @Component
 @RequiredArgsConstructor
@@ -34,12 +33,19 @@ public class FAACCRecommendationStrategy implements ProductRecommendationStrateg
 
         List<FavorRecommendationDTO> recommendationDTO = getRecommendationDTO(userId);
 
-        validateAnswerSize(recommendationDTO);
+        //        validateAnswerSize(recommendationDTO);
 
-        List<Product> firstFindProducts =
-                firstStep(categoryName, recommendationDTO.get(0).getAnswer());
+        List<Product> firstStep = firstStep(categoryName, recommendationDTO.get(0).getAnswer());
 
-        return secondStep(firstFindProducts, recommendationDTO.get(1).getAnswer());
+        if (recommendationDTO.size() == 2) { // 액서서리의 종류를 한가지만 고르면 size가 2
+            return getConditionalProducts(firstStep, recommendationDTO.get(1).getAnswer());
+        } else { // 아니라면 3
+            List<String> answers =
+                    List.of(
+                            recommendationDTO.get(1).getAnswer(),
+                            recommendationDTO.get(2).getAnswer());
+            return secondStep(firstStep, answers);
+        }
     }
 
     @Override
@@ -72,18 +78,49 @@ public class FAACCRecommendationStrategy implements ProductRecommendationStrateg
     }
 
     private List<Product> firstStep(String categoryName, String answer) {
-        return productAdaptor.queryAllByCategoryNameAndCharacter(categoryName, answer);
-    }
-
-    private List<Product> secondStep(List<Product> products, String answer) {
-        return products.stream()
-                .filter(product -> product.getCharacteristic().contains(answer))
-                .toList();
-    }
-
-    private void validateAnswerSize(List<FavorRecommendationDTO> dto) {
-        if (dto.size() != ANSWER_CNT) {
-            throw new BaseException(QuestionException.FAVOR_ANSWER_SIZE_NOT_MATCHED_CATEGORY_ERROR);
+        String[] splitAnswer = answer.split(", ");
+        if (splitAnswer.length == 1) {
+            return productAdaptor.queryAllByCategoryNameAndCharacter(categoryName, splitAnswer[0]);
+        } else {
+            return Stream.concat(
+                            productAdaptor
+                                    .queryAllByCategoryNameAndCharacter(
+                                            categoryName, splitAnswer[0])
+                                    .stream(),
+                            productAdaptor
+                                    .queryAllByCategoryNameAndCharacter(
+                                            categoryName, splitAnswer[1])
+                                    .stream())
+                    .toList();
         }
     }
+
+    private List<Product> secondStep(List<Product> productList, List<String> answers) {
+        List<Product> result = new ArrayList<>();
+        answers.forEach(answer -> result.addAll(getConditionalProducts(productList, answer)));
+        return result;
+    }
+
+    private List<Product> getConditionalProducts(List<Product> productList, String answer) {
+        String[] splitAnswer = answer.split(", ");
+        if (splitAnswer.length == 1) {
+            return productList.stream()
+                    .filter(product -> product.getCharacteristic().contains(answer))
+                    .toList();
+        } else {
+            return productList.stream()
+                    .filter(
+                            product ->
+                                    product.getCharacteristic().contains(splitAnswer[0])
+                                            || product.getCharacteristic().contains(splitAnswer[1]))
+                    .toList();
+        }
+    }
+
+    //    private void validateAnswerSize(List<FavorRecommendationDTO> dto) {
+    //        if (dto.size() != ANSWER_CNT) {
+    //            throw new
+    // BaseException(QuestionException.FAVOR_ANSWER_SIZE_NOT_MATCHED_CATEGORY_ERROR);
+    //        }
+    //    }
 }
