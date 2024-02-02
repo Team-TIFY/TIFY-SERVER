1ab4442f73a545d9eb4991271b47b3b35bb126d6
bongsh0112 <bongsh0112@gmail.com>
2024-02-02 15:42:09 +0900

    FCTOP 추천 전략 로직 수정 #138

--- Domain/src/main/java/tify/server/domain/domains/question/strategy/FCTOPRecommendationStrategy.java
+++ Domain/src/main/java/tify/server/domain/domains/question/strategy/FCTOPRecommendationStrategy.java
@@ -71,19 +71,46 @@ public class FCTOPRecommendationStrategy implements ProductRecommendationStrateg
         List<String> split = Arrays.stream(initFavorAnswer.getAnswerContent().split(", ")).toList();
         favorAnswers.add(initFavorAnswer);
         split.forEach(
-                s ->
+                s -> {
+                    if (s.equals("티셔츠") || s.equals("맨투맨") || s.equals("니트")) {
                         favorAnswers.add(
                                 favorAnswerAdaptor.searchByCategoryNameAndNumber(
-                                        userId, CATEGORY_NAME, map.get(s))));
+                                        userId, CATEGORY_NAME, map.get(s)));
+                    }
+                });
+        favorAnswers.add(
+                favorAnswerAdaptor.searchByCategoryNameAndNumber(userId, CATEGORY_NAME, 6L));
+
         return favorAnswers.stream().map(FavorRecommendationDTO::from).toList();
     }
 
     // 1번째 스텝
     private List<Product> firstStep(String categoryName, String answer) {
-        return productAdaptor.queryAllByCategoryNameAndCharacter(categoryName, answer);
+        List<Product> productList = new ArrayList<>();
+        List<String> splitAnswer = Arrays.stream(answer.split(", ")).toList();
+        if (splitAnswer.size() == 1) {
+            productList.addAll(
+                    productAdaptor.queryAllByCategoryNameAndCharacter(
+                            categoryName, splitAnswer.get(0)));
+        } else {
+            productList.addAll(
+                    productAdaptor.queryAllByCategoryNameAndCharacter(
+                            categoryName, splitAnswer.get(0)));
+            productList.addAll(
+                    productAdaptor
+                            .queryAllByCategoryNameAndCharacter(categoryName, splitAnswer.get(1))
+                            .stream()
+                            .filter(
+                                    product ->
+                                            !productList.contains(
+                                                    product)) // 위에서 추가한 상품과 중복되는 상품 제거
+                            .toList());
+        }
+        return productList;
     }
 
     // 2번째 스텝
+    // Todo: 티셔츠 -> 티로 변경 필요할 듯?
     private List<Product> secondStep(List<Product> products, String answer) {
         List<String> splitAnswer = Arrays.stream(answer.split(", ")).toList();
         return products.stream()
@@ -93,22 +120,21 @@ public class FCTOPRecommendationStrategy implements ProductRecommendationStrateg
                                 return product.getCharacteristic().contains(splitAnswer.get(0));
                             } else {
                                 return product.getCharacteristic().contains(splitAnswer.get(0))
-                                        && product.getCharacteristic().contains(splitAnswer.get(1));
+                                        || (!product.getCharacteristic()
+                                                        .contains(splitAnswer.get(0))
+                                                && product.getCharacteristic()
+                                                        .contains(splitAnswer.get(1)));
                             }
                         })
                 .toList();
     }
 
-    // Todo: 티셔츠 -> 티로 변경 필요할 듯?
     // 3번째 스텝
     private List<Product> thirdStep(List<Product> products, String answer) {
         List<String> splitAnswer = Arrays.stream(answer.split(", ")).toList();
-        if (!splitAnswer.contains("크롭")) {
+        if (!splitAnswer.contains("크롭") && !splitAnswer.contains("상관없음")) {
             return products.stream()
-                    .filter(
-                            product -> {
-                                return !product.getCharacteristic().contains("크롭");
-                            })
+                    .filter(product -> !product.getCharacteristic().contains("크롭"))
                     .toList();
         } else {
             return products;
@@ -119,7 +145,18 @@ public class FCTOPRecommendationStrategy implements ProductRecommendationStrateg
     private List<Product> fourthStep(List<Product> products, String answer) {
         List<String> splitAnswer = Arrays.stream(answer.split(", ")).toList();
         return products.stream()
-                .filter(product -> splitAnswer.contains(product.getCharacteristic()))
+                .filter(
+                        product -> {
+                            if (splitAnswer.size() == 1) {
+                                return product.getCharacteristic().contains(splitAnswer.get(0));
+                            } else {
+                                return product.getCharacteristic().contains(splitAnswer.get(0))
+                                        || (!product.getCharacteristic()
+                                                        .contains(splitAnswer.get(0)))
+                                                && product.getCharacteristic()
+                                                        .contains(splitAnswer.get(1));
+                            }
+                        })
                 .toList();
     }
 }
